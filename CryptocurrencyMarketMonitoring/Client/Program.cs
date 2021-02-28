9f28dfb1f78fba57f4645a2cce77da1ce832229f
Daniel Večeř <vecer@cross.cz>
2021-02-28 01:16:40 +0100

    Add project files.

---
+++ CryptocurrencyMarketMonitoring/Client/Program.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Syncfusion.Blazor;
+
+namespace CryptocurrencyMarketMonitoring.Client
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+			builder.Services.AddSyncfusionBlazor();
+            builder.RootComponents.Add<App>("#app");
+
+            Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("NDA0MTUwQDMxMzgyZTM0MmUzMEpKVDI5bDh6aTFYbEdGZi9NelZ1MnFKa2V0WmFGZE5PUmR1bFJ2M0pDNzg9");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddSyncfusionBlazor();
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
